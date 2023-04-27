a2ee5d739809e36fdf249683a82bf945e614c322
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-04-27 15:01:30 +0300

    addictions to lock box function(add stack of boxes to be opened)

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,24 +8,27 @@ def canUnlockAll(boxes):
     '''
     method that determines if all the boxes can be opened.
     '''
-    # set to store the keys that have been found
+    # Initialize variables
+
+    # Set of keys that have been found
     found_keys = set([0])
-    # stack of boxes to be opened
+
+    # Stack of boxes to be opened
     boxes_to_open = [0]
 
-    # iterate through the boxes to be opened
+    # Continue opening boxes as long as there are boxes to be opened
     while boxes_to_open:
-        # get the keys from the current box
-        keys = boxes[boxes_to_open.pop()]
+        # Get the keys from the current box
+        box_to_open = boxes_to_open.pop()
+        keys = boxes[box_to_open]
 
-        # iterate through the keys
+        # Iterate through the keys
         for key in keys:
-            # check if the key has not been found
+            # Check if the key has not been found
             if key not in found_keys:
-                # add the key to the set of found keys
+                # Add the key to the set of found keys
                 found_keys.add(key)
-                # add the box to the stack of boxes to be opened
+                # Add the box to the stack of boxes to be opened
                 boxes_to_open.append(key)
-
-    # check if all the boxes have been opened
+    # Check if all boxes were opened
     return len(found_keys) == len(boxes)

f081e9758b83f14c5dbcd5f3e24ae0aec9bb3eda
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-05-13 14:27:52 +0300

    print to stderr when format is wrong

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,49 +1,44 @@
 #!/usr/bin/python3
-"""
-Log parsing
-"""
-
+'''
+File: 0-stats.py
+'''
 import sys
 
 
 def print_stats(total_size, status_codes):
-    """
-    Print the statistics
-    """
+    '''
+    script that reads stdin line by line and computes metrics
+    '''
     print("File size: {}".format(total_size))
-    for k in sorted(status_codes.keys()):
-        if status_codes[k] > 0:
-            print("{}: {}".format(k, status_codes[k]))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print("{}: {}".format(code, status_codes[code]))
 
 
-if __name__ == '__main__':
-    count = 0
+if __name__ == "__main__":
     total_size = 0
     status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
                     403: 0, 404: 0, 405: 0, 500: 0}
+    count = 0
 
     try:
-        for line in sys.stdin:
+        for line_number, line in enumerate(sys.stdin):
             try:
                 tokens = line.split()
-                if len(tokens) >= 2:
-                    file_size = int(tokens[-1])
-                    status_code = int(tokens[-2])
-                    if status_code in status_codes:
-                        total_size += file_size
-                        status_codes[status_code] += 1
-                        count += 1
-                else:
-                    raise ValueError("Invalid format")
-            except ValueError as e:
-                print("Error: {}".format(e))
-                continue
-
-            if count % 10 == 0:
-                print_stats(total_size, status_codes)
-
-        if count > 0:
-            print_stats(total_size, status_codes)
-
+                file_size = int(tokens[-1])
+                status_code = int(tokens[-2])
+                if status_code in status_codes:
+                    total_size += file_size
+                    status_codes[status_code] += 1
+                    count += 1
+                    if count == 10:
+                        print_stats(total_size, status_codes)
+                        count = 0
+            except (ValueError, IndexError):
+                print("Skipping line {}: incorrect format"
+                      .format(line_number), file=sys.stderr)
+                pass
     except KeyboardInterrupt:
-        print_stats(total_size, status_codes)
+        pass
+
+    print_stats(total_size, status_codes)

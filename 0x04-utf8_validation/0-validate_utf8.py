d169d89aaac596dbeebb4b915b10f199897f7b24
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-05-18 21:12:31 +0300

    utf-8 validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'''
+File: 0-validate_utf8.py
+'''
+
+
+def validUTF8(data):
+    '''
+    method that determines if a given data set represents a valid UTF-8 encodin
+    '''
+    num_bytes = 0
+
+    for byte in data:
+        if num_bytes == 0:
+            if (byte >> 5) == 0b110:
+                num_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                num_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                num_bytes = 3
+            elif (byte >> 7) != 0:
+                return False
+        else:
+            if (byte >> 6) != 0b10:
+                return False
+            num_bytes -= 1
+
+    return num_bytes == 0
---
+++ 0x04-utf8_validation/README.md
@@ -0,0 +1,7 @@
+# 0x04. UTF-8 Validation
+### Algorithm Python
+ `By`: `Carrie Ybay, Software Engineer at Holberton School`
+ `Weight`: `1`
+ Project will start May 15, 2023 6:00 AM, must end by May 19, 2023 6:00 AM
+ Checker was released at May 16, 2023 6:00 AM
+ An auto review will be launched at the deadline

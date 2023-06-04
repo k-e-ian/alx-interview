77e031ea0bcd269568f5c43f3db3e0dc5ca9911d
Ian Kitembe <ianedwin@outlook.com>
2023-06-04 08:23:33 -0700

    Return to jedi

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -9,13 +9,29 @@ request(apiUrl, (error, response, body) => {
     const movie = JSON.parse(body);
     const characterUrls = movie.characters;
 
-    characterUrls.forEach((characterUrl) => {
+    const fetchCharacter = (characterUrl) => {
       request(characterUrl, (error, response, body) => {
         if (!error && response.statusCode === 200) {
           const character = JSON.parse(body);
           console.log(character.name);
         }
       });
-    });
+    };
+
+    const fetchAllCharacters = (urls) => {
+      if (urls.length === 0) {
+        return;
+      }
+
+      const characterUrl = urls.shift();
+      fetchCharacter(characterUrl);
+
+      // Fetch the next character after a short delay to avoid API rate limits
+      setTimeout(() => {
+        fetchAllCharacters(urls);
+      }, 100);
+    };
+
+    fetchAllCharacters(characterUrls);
   }
 });

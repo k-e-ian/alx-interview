4f40db247ee1452a4fde9a26484bd3dcba357400
Ian Kitembe <ianedwin@outlook.com>
2023-06-04 08:18:11 -0700

    Star wars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,35 +1,21 @@
 #!/usr/bin/node
-
 const request = require('request');
 
-// Function to fetch movie characters
-function getMovieCharacters(movieId) {
-  const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-  request(apiUrl, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      const film = JSON.parse(body);
-      const characterUrls = film.characters;
-
-      // Fetch each character and print their names
-      characterUrls.forEach((characterUrl) => {
-        request(characterUrl, (err, resp, characterBody) => {
-          if (!err && resp.statusCode === 200) {
-            const character = JSON.parse(characterBody);
-            console.log(character.name);
-          } else {
-            console.log('Error:', err);
-          }
-        });
-      });
-    } else {
-      console.log('Error:', error);
-    }
-  });
-}
-
-// Get the movie ID from the command line argument
 const movieId = process.argv[2];
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
-// Call the function to get movie characters
-getMovieCharacters(movieId);
+request(apiUrl, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const movie = JSON.parse(body);
+    const characterUrls = movie.characters;
+
+    characterUrls.forEach((characterUrl) => {
+      request(characterUrl, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          const character = JSON.parse(body);
+          console.log(character.name);
+        }
+      });
+    });
+  }
+});

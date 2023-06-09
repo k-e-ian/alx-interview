15df03bcff3efdd4b23223ff43caf22fd177fef1
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-06-09 05:56:10 +0300

    2d matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3.8
+'''
+File: 0-rotate_2d_matrix.py
+'''
+
+def rotate_2d_matrix(matrix):
+    '''
+    Rotate matrix 9- degrees
+    '''
+    n = len(matrix)  # Get the size of the matrix
+
+    # Step 1: Transpose the matrix
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # Step 2: Reverse each row
+    for i in range(n):
+        matrix[i] = matrix[i][::-1]
---
+++ 0x07-rotate_2d_matrix/README.md
@@ -0,0 +1,7 @@
+# 0x07. Rotate 2D Matrix
+### Algorithm Python
+ `By`: `Jesse Hedden, Software Engineer`
+ Weight: 1
+ Project will start Jun 5, 2023 6:00 AM, must end by Jun 9, 2023 6:00 AM
+ Checker was released at Jun 6, 2023 6:00 AM
+ An auto review will be launched at the deadline
